6fa4565c5c3068f9a3e9cf22eec6769aa4a039c2
Akm Kawser <kawserrabbi3@gmail.com>
2019-10-21 04:31:42 +0600

    Create 1.Object-Class.py

---
+++ 1.Object-Class.py
@@ -0,0 +1,40 @@
+#task-1
+
+class Student:
+    def _init_(self,idd,name,cgpa):
+        self.idd=idd
+        self.name=name
+        self.cgpa=cgpa
+
+    def setIdd (i):
+        idd=i
+    def setName (n):
+        name=n
+    def setCgpa (c):
+        cgpa=c
+
+
+    def getIdd():
+        return idd
+    def getName():
+        return name
+    def getCgpa():
+        return cgpa
+
+
+s=Student()
+s.setIdd(15)
+s.setName("Kawser")
+s.setCgpa(4)
+
+print(s.getIdd)
+print(s.getName)
+print(s.getCgpa)
+    
+        
+        
+     
+    
+    
+
+
